489de4bfd98b6226402bbd81fd55a64a90d5ea8e
Sertac Poyraz <sertac.poyraz@gricreative.com>
2017-05-29 16:06:56 +0300

    es6 and babel integration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const express = require('express');
+import express from 'express'; // babel version
+// es6 version
+// const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
