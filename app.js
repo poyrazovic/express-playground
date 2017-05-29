7b794d8ede49a92fbbd8a86b93f2894a7823667d
Sertac Poyraz <sertac.poyraz@gricreative.com>
2017-05-29 14:36:29 +0300

    first server start

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World!');
+});
+
+app.listen('9005', function () {
+    console.log('Server Started!');
+});
